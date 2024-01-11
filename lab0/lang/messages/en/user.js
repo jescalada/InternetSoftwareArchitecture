289adfb8bccb6cc3d6d0633af65e40a4fcb52576
Juan Escalada <juanescalada175@gmail.com>
2024-01-11 08:09:20 -0800

    Add localization

---
+++ lab0/lang/messages/en/user.js
@@ -0,0 +1,21 @@
+// Contains all the strings that are used in the app
+// This file is used by the app to display the strings in the correct language
+
+export const user = {
+
+  // General
+  "StudentName": "Juan Escalada",
+  "StudentID": "A01285535",
+
+  // Lab 0
+  "lab0": {
+    "PageTitle": "Lab 0",
+    "Header": "Lab 0",
+    "Label": "How many buttons to create?",
+    "SubmitButtonText": "Go!",
+    "StartMessage": "Guess the order of the buttons!",
+    "WinMessage": "Excellent memory!",
+    "LoseMessage": "Wrong order!"
+  }
+
+}
