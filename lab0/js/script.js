28da9af25e52743334c0b76ccc0f7439412b0cc1
Juan Escalada <juanescalada175@gmail.com>
2024-01-10 18:29:56 -0800

    Add basic game elements

--- lab0/js/script.js
+++ lab0/js/script.js
@@ -15,3 +15,63 @@ function Button(color, width, height, top, left, order) {
 
   this.setLocation(top, left);
 }
+
+function reset() {
+  arrayButtons = [];
+  currentOrder = 0;
+  interval = null;
+  document.getElementById('container').innerHTML = "";
+  document.getElementById('submit').disabled = false;
+}
+
+function updateButtons() {
+  for (let i = 0; i < arrayButtons.length; i++) {
+    randomizeButtonPosition(i);
+  }
+  showButtons();
+}
+
+function showButtons() {
+  /**
+   * Displays all buttons on the page
+   */
+  let container = document.getElementById('container');
+  container.innerHTML = "";
+  arrayButtons.forEach(function (button) {
+    container.appendChild(button.btn);
+  });
+}
+
+function randomizeButtonPosition(index) {
+  let windowWidth = getCurrentWindowWidth();
+  let windowHeight = getCurrentWindowHeight();
+  let button = arrayButtons[index];
+  let xLimit = windowWidth - parseInt(button.btn.style.width) * 16;
+  let yLimit = windowHeight - parseInt(button.btn.style.height) * 16;
+  
+  arrayButtons[index].setLocation(
+    Math.floor(Math.random() * yLimit) + "px",
+    Math.floor(Math.random() * xLimit) + "px"
+  );
+}
+
+function getCurrentWindowWidth() {
+  return window.innerWidth;
+}
+
+function getCurrentWindowHeight() {
+  return window.innerHeight;
+}
+
+/**
+ * Submits the input to start the game
+ */
+function go() {
+  let valid = validateInput();
+  if (valid) {
+    document.getElementById('submit').disabled = true;
+    generateInitialButtons(document.getElementById('input').value);
+    showButtons();
+    startGame();
+  }
+}
