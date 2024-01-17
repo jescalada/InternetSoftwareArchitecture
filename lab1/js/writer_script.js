36971c2c6c5174a0389c0fbe14c3b078c5255371
Juan Escalada <juanescalada175@gmail.com>
2024-01-17 15:16:45 -0800

    Add create note dialog triggers

--- lab1/js/writer_script.js
+++ lab1/js/writer_script.js
@@ -3,13 +3,13 @@ import { Note } from "./note.js";
 function addNote() {
   let title = document.getElementById('title').value;
   let content = document.getElementById('content').value;
-  let id = Date.now();
 
-  let note = new Note(title, content, id);
+  let note = new Note(title, content);
 
   addNoteToLocalStorage(note);
   addNoteToList(note);
   console.log("Added: ", note.toString());
+  hideCreateContainer();
 }
 
 function addNoteToList(note) {
@@ -62,8 +62,18 @@ function removeNoteFromLocalStorage(noteId) {
   localStorage.setItem('notes', JSON.stringify(notes));
 }
 
+function showCreateContainer() {
+  document.getElementById('create-post-container').style.display = 'block';
+}
+
+function hideCreateContainer() {
+  document.getElementById('create-post-container').style.display = 'none';
+}
+
 function setup() {
   document.getElementById('submit').addEventListener('click', addNote);
+  document.getElementById('add').addEventListener('click', showCreateContainer);
+  hideCreateContainer();
 }
 
 setup();
