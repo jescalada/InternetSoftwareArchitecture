4ad8a303f399830655837e1bb1c9a136f77e9963
Juan Escalada <juanescalada175@gmail.com>
2024-01-17 13:56:40 -0800

    Add basic localization file

---
+++ lab1/lang/messages/en/user.js
@@ -0,0 +1,14 @@
+// Contains all the strings that are used in the app
+// This file is used by the app to display the strings in the correct language
+
+export const user = {
+  // General
+  "StudentName": "Juan Escalada",
+  "StudentID": "A01285535",
+
+  // Lab 1
+  "lab1": {
+    "PageTitle": "Lab 1 - Note Taking App",
+    "Header": "Lab 1 - Note Taking App",
+  }
+}
