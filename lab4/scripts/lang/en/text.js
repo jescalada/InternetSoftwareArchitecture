171d9f1effe0895a8968dd9b9dae14f3bafa74f2
Juan Escalada <juanescalada175@gmail.com>
2024-02-17 10:20:17 -0800

    Add localization

---
+++ lab4/scripts/lang/en/text.js
@@ -0,0 +1,13 @@
+// Contains all the strings that are used in the app
+// This file is used by the app to display the strings in the correct language
+export const user = {
+    "StudentNames": "Braden Rogers & Juan Escalada",
+  
+    "SearchAWord": "Search a word",
+    "Search": "Search",
+    "AddAWord": "Add a word",
+    "QueryAWord": "Query a word",
+    "StoreANewWord": "Store a new word",
+    "TotalRequestsReceived": "Total requests received:",
+    "Submit": "Submit",
+}
